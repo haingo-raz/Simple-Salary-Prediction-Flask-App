601b91c591e39ba6b80e6848ad6db729f33682ac
Haingo <100688816+haingo-raz@users.noreply.github.com>
2024-09-07 19:02:06 +0000

    Organize, rename and clean files

--- app.py
+++ app.py
@@ -18,10 +18,6 @@ def predict():
 	final_feature = np.array([feature]).reshape(-1, 1)
 	prediction = model.predict(final_feature)
 	return render_template('index.html', prediction_text='Predicted Salary: ${:.2f}'.format(prediction[0]))
-	# features = [float(x) for x in request.form.values()]
-	# final_features = np.array(features).reshape(1, -1)
-	# prediction = model.predict(final_features)
-	# return render_template('index.html', prediction_text='Predicted salary:'.format(prediction[0]))
 
 if __name__ == '__main__':
 	app.run(debug=False, host='0.0.0.0')
